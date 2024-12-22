928ea095bbc5ee22a74d2846ec1d54b92842d37b
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-12-22 22:05:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 "use strict";
 
+const ladder = {
+  step: 0,
+  
+  up: function () {
+    this.step++;
+    return this; 
+  },
+  
+  down: function () {
+    this.step--;
+    return this; 
+  },
+  
+  showStep: function () {
+    console.log(this.step); 
+    return this; 
+  }
+};
+
+ladder.up().up().down().showStep(); // 1
+
+
+
 
 
 
